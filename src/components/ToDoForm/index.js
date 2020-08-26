4527deea062b8b3d218b1c30bb0f322fb54d7e23
SamHo <sam.ho@kdanmobile.com>
2020-08-26 13:36:44 +0800

    Add feature: prevent dirty words

--- src/components/ToDoForm/index.js
+++ src/components/ToDoForm/index.js
@@ -2,16 +2,28 @@ import React, { useState, useEffect } from 'react';
 
 const ToDoForm = ({addTask}) => {
   const [value, setValue] = useState('new task');
+  const dirty_words = ["fuck", "shit", "damn"]
 
   const handleForm = (e) => {
     e.preventDefault();
     addTask(value);
   }
 
+  const onChangeListener = (e) => {
+    setValue(e.target.value)
+  }
+
   return (
     <form onSubmit={handleForm}>
-      <input type="text" onChange={(e) => {setValue(e.target.value)}}/>
-      <input type="submit" value="新增"/>
+      <input type="text" onChange={onChangeListener}/>
+      {(()=>{
+        if (dirty_words.includes(value.toLowerCase())) {
+          return <input type="submit" value="新增" disabled/>
+        } else {
+          return <input type="submit" value="新增"/>
+        }
+      })()}
+
     </form>
   );
 };
